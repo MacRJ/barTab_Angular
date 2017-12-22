e922265b33231817c5ecb01dc4d16017a8645d5c
macRJ <rmacwj@gmail.com>
2017-12-21 17:53:44 -0700

    tabs now come in colors

--- modules/tabSection.js
+++ modules/tabSection.js
@@ -1,8 +1,39 @@
 const tabSection = angular.module('tabSectionMod', [
   'factory'
 ])
+ var count = 0;
  tabSection.controller('tabSectionController', function($scope, tabs) {
    tabs.list(function(tabs) {
      $scope.tabs = tabs;
    })
+
+   $scope.displayTab = function(selected){
+
+     var colors = ['#33a1fd','#52dbaf','#fdca40','#f79824','#33a1fd','#52dbaf','#fdca40','#f79824','#33a1fd','#52dbaf','#fdca40','#f79824','#2176ff','#32ffd2'];
+
+
+     if(count === 14){
+       count = 0
+     } else {
+       count++
+     }
+
+     let notSelectedCss = {
+       "height": "40px",
+       "padding-top": "5px",
+       "margin-left": "40px",
+       "margin-top": "5px",
+       "background-color": colors[count]
+
+     };
+     let selectedCss = {
+       "height": "40px",
+       "padding-top": "5px",
+       "margin-left": "15px",
+       "margin-top": "5px",
+       "background-color": colors[count]
+
+     };
+    return selected ? selectedCss : notSelectedCss
+  }
 })
